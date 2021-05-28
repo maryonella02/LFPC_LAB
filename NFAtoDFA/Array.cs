185ac44b1564975bbdad89a6b2645e60cc4cc651
Marinela <maryonella02@gmail.com>
2021-05-28 10:24:41 +0300

    add lab 1 and lab 2

---
+++ NFAtoDFA/Array.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace NFAtoDFA
+{
+    internal static class Array
+    {
+        public static string[][] RectangularStringArray(int size1, int size2)
+        {
+            string[][] newArray = new string[size1][];
+            for (int array1 = 0; array1 < size1; array1++)
+            {
+                newArray[array1] = new string[size2];
+            }
+            return newArray;
+        }
+        public static void Convert()
+        {
+            string[][] DFA_table = Array.RectangularStringArray(1000, num_of_symbols);
+            string[] final_states_DFA = new string[1000];
+            int nfa_final_states = 0;
+            Dictionary<char, int> new_states;
+            for (int i = 0; q.Count > 0; i++)
+            {
+                string current_state = q.RemoveFirst();
+                Console.Write(current_state + "|\t");
+                for (int j = 0; j < num_of_symbols; j++)
+                {
+                    DFA_table[i][j] = "";
+                    if (current_state.Length == 1 && !states.ContainsKey(current_state[0]))
+                    {
+                        DFA_table[i][j] = current_state;
+                        if (!map.ContainsKey(DFA_table[i][j]))
+                        {
+                            q.AddLast(DFA_table[i][j]);
+                            map[DFA_table[i][j]] = 1;
+                        }
+                        if (final_states.ContainsKey((DFA_table[i][j][0])))
+                        {
+                            final_states_DFA[nfa_final_states++] = DFA_table[i][j];
+                        }
+                        Console.Write(DFA_table[i][j] + "\t");
+                        continue;
+                    }
+                    int new_final_state = 0;
+                    new_states = new Dictionary<char, int>();
+                    for (int k = 0; k < current_state.Length; k++)
+                    {
+                        if (!states.ContainsKey(current_state[k]))
+                        {
+                            continue;
+                        }
+                        if (final_states.ContainsKey(current_state[k]))
+                        {
+                            new_final_state = 1;
+                        }
+                        for (int ch = 0; ch < NFA_table[states[current_state[k]]][j].Length; ch++)
+                        {
+                            if (!new_states.ContainsKey(NFA_table[states[current_state[k]]][j][ch]) && states.ContainsKey(NFA_table[states[current_state[k]]][j][ch]))
+                            {
+                                DFA_table[i][j] += NFA_table[states[current_state[k]]][j][ch];
+                                new_states[NFA_table[states[current_state[k]]][j][ch]] = 1;
+                            }
+                        }
+                    }
+
+                    if (!map.ContainsKey(DFA_table[i][j]))
+                    {
+                        q.AddLast(DFA_table[i][j]);
+                        map[DFA_table[i][j]] = 1;
+                    }
+                    if (new_final_state == 1)
+                    {
+                        final_states_DFA[nfa_final_states++] = DFA_table[i][j];
+                    }
+                    Console.Write(DFA_table[i][j] + "\t");
+                }
+            }
+        }
+    }
+
+}
+
---
+++ NFAtoDFA/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace NFAtoDFA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num_of_states = 3;
+            char[] states_ar = new char[] { 'A', 'B', 'C' };
+            Dictionary<char, int> states = new Dictionary<char, int>();
+            states['A'] = 0;
+            states['B'] = 1;
+            states['C'] = 2;
+
+            char initial_state = 'A';
+            Dictionary<char, int> final_states = new Dictionary<char, int>();
+            final_states['C'] = 1;
+
+            int num_of_symbols = 2;
+            char[] alphabet = new char[] { 'a', 'b' };
+
+            LinkedList<string> q = new LinkedList<string>();
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            q.AddLast(initial_state + "");
+            map[initial_state + ""] = 1;
+
+            string[][] NFA_table = Array.RectangularStringArray(num_of_states, num_of_symbols);
+
+            Console.Write("\n\t");
+            for (int al = 0; al < num_of_symbols; al++)
+            {
+                Console.Write(alphabet[al] + "\t");
+            }
+            Console.WriteLine();
+
+            string[,] NFA_table = new string[3, 2] { { "AB", "-" }, { "C", "B" },
+                                        { "A", "C" } };
+            Console.WriteLine("\nEquivalent DFA Table: ");
+            Array.Convert();
+
+           
+        }
+    }
+
+}
+
